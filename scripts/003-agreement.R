b1c5a11a3501a37a5d93b6af98356ad163a147cd
Simon Nolte <simonnolte@googlemail.com>
2025-07-25 09:30:06 +0200

    Select studies for parallel extraction

---
+++ scripts/003-agreement.R
@@ -0,0 +1,70 @@
+## Script for the meta-pre-post project
+## Script #3: Extraction Agreement
+## written by Simon Nolte in 07/2025
+
+# This script organizes and analyzing the parallel extraction procedure
+
+#------------------------------------------------------------------------------
+# PART 1: Study Selection
+
+# Regarding parallel extraction, the preregistration states: "We perform a
+# validation stage to check for potential issues in the data extraction process.
+# On a subset of the first 20 studies of data extraction, we perform an
+# additional test of extraction reliability by having two independent
+# researchers perform the data extraction (instead of one extractor and one
+# validator). The two extractors will resolve conflicts by discussion."
+
+# At this stage of the project (23-07-2025) all studies have been extracted once
+# with a few studies already validated. 
+
+# Based on the preregistration, the first 20 studies would need to be 
+# extracted in parallel for the reliability determination. But a better
+# procedure would be to randomly choose 20 studies. A totally random
+# selection is not useful because (1) some studies already have validation
+# entries, so we will not use them for parallel extraction, (2) to limit the
+# burden to co-authors and ensure to accurately capture the reliability of the
+# persons who performed the most extraction, every study will be extracted at
+# least once by the primary investigator (SN). This mean that all studies not
+# extracted by SN will be subject to the parallel extraction.
+
+# Therefore, we will choose the following studies for the parallel extraction:
+# - All studies that have not been extracted by SN so far.#
+# - Plus a random subset of all studies extracted by SN that have not received a
+# validation so far (until a final number of 20 parallel extractions is 
+# reached)
+
+# import current extraction data
+e <- read.csv("backup/extraction-23-07-25.csv")
+# import current validation data
+i <- read.csv("backup/validation-23-07-25.csv")
+
+# show number of extractions per user
+table(e$extract_user)
+
+# select ids of extraction performed by other user than SN
+ids <- e$study_id[e$extract_user != "SN"]
+ids
+#> 2, 18, 56, 95, 96, 101, 109, 126
+# number of extractions not performed by SN
+length(ids)
+#> 8 
+
+# get the study IDs that may be used for the parallel extraction
+e_SN <- e[e$extract_user == "SN",]
+pool <- e_SN$study_id[!e_SN$study_id %in% i$study_id]
+
+# randomly select the needed number of studies from this pool:
+set.seed(4711)
+sam <- sample(pool, 20 - length(ids), replace = FALSE)
+
+p_ids <- c(ids, sam)
+# these are the final study IDs used for the parallel extraction:
+sort(p_ids)
+#> 2, 18, 36, 40, 51, 56, 90, 92, 95, 96, 101, 109, 120, 126, 134, 136, 140,
+#> 158, 180, 183
+
+# The data validations of these will be marked as "Study selected for parallel 
+# extraction. No validation needed."
+
+#-----------------------------------------------------------------------------
+# PART 2: Agreement Analysis
